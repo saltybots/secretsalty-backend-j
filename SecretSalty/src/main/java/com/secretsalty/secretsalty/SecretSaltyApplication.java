6c47018024197e375c693beb8dd915bc44b4baef
zlemyztex <er.denis@pm.me>
2023-12-21 16:02:44 +0100

    spring project init

---
+++ SecretSalty/src/main/java/com/secretsalty/secretsalty/SecretSaltyApplication.java
@@ -0,0 +1,13 @@
+package com.secretsalty.secretsalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSaltyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretSaltyApplication.class, args);
+    }
+
+}
---
+++ SecretSalty/src/test/java/com/secretsalty/secretsalty/SecretSaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretsalty.secretsalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSaltyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
